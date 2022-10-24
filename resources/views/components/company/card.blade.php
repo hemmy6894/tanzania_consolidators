44251865192e512473ea5a957199505c41d1750b
Hemedi Manyinja <hmanyinja@gmail.com>
2022-10-24 10:34:51 +0300

    E consolidator

--- resources/views/components/company/card.blade.php
+++ resources/views/components/company/card.blade.php
@@ -7,7 +7,7 @@
         </div>
         <div class="pt-8 pb-16 px-10 flex-auto relative overflow-hidden">
             <h4 class="text-2xl font-bold mt-0 mb-2">{{ $company->name }}</h4>
-            <p class="text-blueGray-500 leading-relaxed">{{ $company->short_description }}</p>
+            <p class="text-blueGray-500 leading-relaxed">{{ substr($company->short_description, 156) }} ...</p>
             <div
                 class="grid grid-cols-2 text-blueGray-500 border-blueGray-200 opacity-50 rounded-b px-4 py-4 border-t absolute bottom-0 left-0 w-full text-center flex items-center justify-center transition-all duration-300 ease-in-out transform translate-y-0 group-hover:translate-y-0 group-hover:opacity-100">
                 <div><a href="{{ route('company', ['company' => $company->slag]) }}">Read more</a></div>
